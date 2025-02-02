e244e5eaa442cf5a210607eb552cca5a9a085d7e
Juan Francisco Mosquera <maxiplux@gmail.com>
2025-02-02 11:42:20 -0500

    Initial commit

---
+++ src/main/java/app/quantun/summay/SummayApplication.java
@@ -0,0 +1,13 @@
+package app.quantun.summay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/quantun/summay/TestSummayApplication.java
@@ -0,0 +1,11 @@
+package app.quantun.summay;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSummayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(SummayApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
